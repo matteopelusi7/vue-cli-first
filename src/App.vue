7f8891c14af02d15293c44ba6c0609c82964c58c
matteopelusi7 <pelusimatteo02@gmail.com>
2022-03-07 16:55:30 +0100

    Add component title

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <TitleCli />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TitleCli from './components/Title.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TitleCli
   }
 }
 </script>
---
+++ src/components/Title.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <h1>La mia prima app con Vue CLI!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TitleCli'
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
